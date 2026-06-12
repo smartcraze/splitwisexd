46d63e01093a83f3573b15753f9324464cb0f994
smartcraze <dev.surajv@gmail.com>
2026-06-12 15:46:07 +0530

    setup the splitwise turborepo

---
+++ packages/db/index.ts
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({
+    adapter,
+    log: [
+        "query",
+        "info",
+        "warn",
+        "error"
+    ]
+});
+
+export { prisma };
